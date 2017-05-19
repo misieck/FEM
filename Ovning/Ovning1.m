afd8e45b271bddadd864c78ccea318e027e1985e
Michal Pomorski <misieck@gmail.com>
2017-05-19 09:01:24 +0200

    ovning using solveeq with boundary

--- Ovning/Ovning1.m
+++ Ovning/Ovning1.m
@@ -1,14 +1,14 @@
     Q = 100;
     A = 10;
     k = 5;
-    Nelem=20;
+    Nelem=40;
     Ndof = Nelem +1;
     L  = 6/Nelem;
-    T0 = 0;
+    T0 = 100;
     q_4_boundary = 15;
+    bc = [1 T0]
     
-    
-    Edof = zeros(Nelem, 3)
+    Edof = zeros(Nelem, 3);
     
     for i=1:Nelem 
         Edof(i,:) = [i, i, i + 1];
@@ -17,13 +17,13 @@
    EssentialNodeNo = 1;
     
    %materialegenskapsmatris
-   K = zeros(Ndof)
+   K = zeros(Ndof);
    
    %lastvektorn
-   fl = zeros(Ndof, 1)
+   fl = zeros(Ndof, 1);
    
    %elementmaterialegenskap
-   ep = k*A/L; %simple
+   ep = k*A/L %simple
    Ke = spring1e(ep) %matrix
    
    %elementlastvektorn
@@ -37,16 +37,22 @@ end
 %cut out essential node from equation
 fb_clipped = [ zeros(Ndof-2, 1); -A*q_4_boundary];
 
-K_clipped = K
+K_clipped = K;
 K_clipped (EssentialNodeNo,:) = [];
-K_clipped (:, EssentialNodeNo) = []
+K_clipped (:, EssentialNodeNo) = [];
 fl_clipped = fl;
-fl_clipped(EssentialNodeNo) = []
-f_clipped = fl_clipped + fb_clipped
+fl_clipped(EssentialNodeNo) = [];
+f_clipped = fl_clipped + fb_clipped;
+
+
 
 
 %Denna borde kallas A
-[T_clipped, Q]=solveq(K_clipped,f_clipped)
-T = [T0;T_clipped]
+%[T_clipped, Q]=solveq(K_clipped,f_clipped)
+%T = [T0;T_clipped]
+[T, Q]=solveq(K,[100;f_clipped], bc);
 
+Q(1)
 q_1_boundary = spring1s( ep, [T(2), T(1)])
+
+T(1:4)
