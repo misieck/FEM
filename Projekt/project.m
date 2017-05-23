5c663956f82b77eee4cee1099a0b19b8bbc26398
MariHeléne <marihelenecarlsson@hotmail.com>
2017-05-23 04:35:30 +0200

    Tidying up the code - old code is commented

--- Projekt/project.m
+++ Projekt/project.m
@@ -1,5 +1,3 @@
-
-
 clear all
 constants;
 
@@ -9,8 +7,10 @@ edges = stuff.edges;
 %correct scale in stuff.points 
 points = stuff.points/1000;
 
+
 elements = stuff.triangles;
 nelem=length(elements(1,:));
+edof = zeros(nelem,4);
 edof(:,1)=1:nelem ;
 edof(:,2:4)=elements(1:3,:)' ;
 coord=points' ;
@@ -74,25 +74,29 @@ for edge = q_edges
     f_b = f_b + f_b_e;
 end
 
-for edge = conv_edges
-    nodes = edges(1:2, edge);
-    edge_length = distance_proj( points(:, edges(1, edge)), points (:, edges(2, edge)) );
-    f_b_e = sparse(ndof, 1);
-    f_b_e(nodes) = T_inf * alpha_c * edge_length/2;
-    f_b = f_b + f_b_e;
-end
+% for edge = conv_edges
+%     nodes = edges(1:2, edge);
+%     edge_length = distance_proj( points(:, edges(1, edge)), points (:, edges(2, edge)) );
+%     f_b_e = sparse(ndof, 1);
+%     f_b_e(nodes) = T_inf * alpha_c * edge_length/2;
+%     f_b = f_b + f_b_e;
+% end
 
 for edge = conv_edges
     nodes = edges(1:2, edge);
-    edge_length = distance_proj( points(:, edges(1, edge)), points (:, edges(2, edge)) );
-    K_c_e = sparse(ndof, ndof);
-    K_c_e (nodes(1), nodes(2)) = alpha_c * edge_length/6;
-    K_c_e (nodes(2), nodes(1)) = K_c_e(nodes(1), nodes(2));
-    K_c_e (nodes(1), nodes(1)) = alpha_c * edge_length/3;
-    K_c_e (nodes(2), nodes(2)) = K_c_e (nodes(1), nodes(1)) ;
-    K_c = K_c + K_c_e;
+     edge_length = distance_proj( points(:, edges(1, edge)), points (:, edges(2, edge)) );
+%     K_c_e = sparse(ndof, ndof);
+%     K_c_e (nodes(1), nodes(2)) = alpha_c * edge_length/6;
+%     K_c_e (nodes(2), nodes(1)) = K_c_e(nodes(1), nodes(2));
+%     K_c_e (nodes(1), nodes(1)) = alpha_c * edge_length/3;
+%     K_c_e (nodes(2), nodes(2)) = K_c_e (nodes(1), nodes(1)) ;
+%     K_c = K_c + K_c_e;
+        
+        f_be = T_inf * alpha_c * edge_length/2*ones(2,1);
+        K_ce = edge_length*alpha_c/6 * [2, 1; 1, 2];
+        [K_c, f_b] = assem([1, nodes(1), nodes(2)], K_c, K_ce, f_b, f_be);
+ 
 end
-diag(K_c)
 
 K = K + K_c;
 
@@ -105,27 +109,29 @@ ip = [dt, Time, 1, [4, ndof, [1 10 60 240], [1:ndof] ]  ];
 [Tsnap D V] = step1(K, C, d0, ip, f_b, []);
 
 
-stationary_index = find_stationary(V, 0.0001, 50);
-stationary_temps = D(:, stationary_index);
+%stationary_index = find_stationary(V, 0.0001, 50);
+%stationary_temps = D(:, stationary_index);
 
 scale = 'auto';
 for i = 1:3
     draw_temps(Ex, Ey, edof, Tsnap(:,i), i+1, scale);
 end
 
-draw_temps(Ex, Ey, edof, stationary_temps, 5, scale);
+%draw_temps(Ex, Ey, edof, stationary_temps, 5, scale);
+astat = solveq(K, f_b);
+draw_temps(Ex, Ey, edof, astat, 5, scale);
 
-
-% Starting temperature
-a0 = T_0*ones(ndof,1);
-% Solve equation system over time
-dt = 1; % Time step
-Kt = C/dt + K;
-for i = 1:250 % Run for 10 s
-    ft = f_b + C*a0/dt;
-    a=solveq(Kt,ft);
-   
-    a0 = a;
-end
+% 
+% % Starting temperature
+% a0 = T_0*ones(ndof,1);
+% % Solve equation system over time
+% dt = 1; % Time step
+% Kt = C/dt + K;
+% for i = 1:250 % Run for 10 s
+%     ft = f_b + C*a0/dt;
+%     a=solveq(Kt,ft);
+%    
+%     a0 = a;
+% end
 
 
