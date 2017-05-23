92e8e9bf4bc74e3a937e2e445cae334904e97fc8
MariHeléne <marihelenecarlsson@hotmail.com>
2017-05-23 04:33:00 +0200

    Modified: draws whole resistor

--- Projekt/draw_temps.m
+++ Projekt/draw_temps.m
@@ -1,7 +1,16 @@
 function draw_temps(Ex, Ey, edof, Temps, figurenr, colormapscale)
+
 figure(figurenr);
+hold on;
 ed=extract(edof,Temps);
-%colormap(hot)
-fill(Ex',Ey',ed')
+colormap('jet')
+fill(Ex',Ey',ed');
+fill(-Ex', Ey', ed');
 colorbar;
 caxis(colormapscale);
+
+% figure(1)
+% hold on
+% ed = extract(edof,astat);
+% fill(Ex', Ey', ed');
+% fill(-Ex', Ey', ed');
