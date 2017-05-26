c2911c92d23ef916ac46e827500b02b11ecec801
Michal Pomorski <misieck@gmail.com>
2017-05-26 12:36:28 +0200

    polish the look of stresses_strains

--- Projekt/stresses_strains.m
+++ Projekt/stresses_strains.m
@@ -12,7 +12,6 @@ ndof = 2*ndof;
 
 %Skapa tom K, f och f0
 K = sparse(ndof,ndof);
-f = sparse(ndof, 1);
 f0 = sparse(ndof, 1);
 
  [E,nu,~, ~, ~, alpha] = decideElementproperties(elements, nelem);
@@ -20,28 +19,28 @@ f0 = sparse(ndof, 1);
 %Calculate D depending on material, and assembla Ke och fe0
 for i = 1:nelem
  
- D = hooke(2, E(i), nu(i));
- D = red(D, 3);
+ De = hooke(2, E(i), nu(i));
+ De = red(De, 3);
  
  %elementets x-koordinater i noderna    
  x = Ex(i,:)';
  %elementets y-koordinater i noderna
  y = Ey(i,:)';
  
- %Vi vill veta vilka noder som �r kopplade...
+ %Vi vill veta vilka noder som ar kopplade...
  nodes = edof_old(i,2:4);
  %för attt kunna plocka ut rätt temperatur ur a_stat
  T = Temp_stat(nodes);
  
  %Räkna ut Ke
- Ke = plante(Ex(i,:), Ey(i,:), [2 1], D);
+ Ke = plante(Ex(i,:), Ey(i,:), [2 1], De);
  
- %Räkna ut f0e, lite OSÄKER PÅ DETTA UTTRYCK!!!!
- 
+ %Räkna ut f0e
  T_avg = sum(T)/3;
- constant=0.5*alpha(i)*E(i)/(1-2*nu(i))*(T_avg-T_0);
- Be110 = [y(2)-y(3); x(3)- x(2); y(3)-y(1); x(1)-x(3); y(1)-y(2); x(2)-x(1)];
- f0e = constant * Be110;
+ a_tmp_constant=0.5*alpha(i)*E(i)/(1-2*nu(i))*(T_avg-T_0);
+ Be_110 = [y(2)-y(3); x(3)- x(2); y(3)-y(1); x(1)-x(3); y(1)-y(2); x(2)-x(1)];
+ f0e = a_tmp_constant * Be_110;
+ 
  [K, f0] = assem(edof(i, :), K, Ke, f0, f0e);
  
 end
@@ -51,71 +50,63 @@ ux0_edge = find(coord(:,1) > 1e-3 - 1e-6);
 
 ux0_middle = find(coord(:,1) <  1e-6);
 
-ux0_bottom = find(coord(:,2)<0 + 1e-6);
+ux0_bottom = find(coord(:,2) <  1e-6);
 uy0_bottom = ux0_bottom + ndof/2;
 
-bc_dof = unique([ux0_edge; ux0_middle; uy0_bottom]);
+ux0_phony = find( coord(:,2) - 0.2*1e-3 < 1e-6  );
+uy0_phony = ux0_phony + ndof/2;
+
 
 %Defines boundary condition
+bc_dof = unique([ux0_middle; uy0_bottom; ux0_edge]);
+%bc_dof = unique([ux0_middle]);
 bc = [bc_dof zeros(length(bc_dof), 1)];
-f = f + f0;
-
 
 %Find displacements
-a = solveq(K,f,bc);
+u = solveq(K,f0,bc);
 
-
-%stress
+%von Mises effective stress, elementwise
 Seff_el = zeros(nelem,1);
-
 for i = 1:nelem
     
-     D = hooke(2, E(i), nu(i));
-     D = red(D, 3);
+    De = hooke(2, E(i), nu(i));
+    De = red(De, 3);
 
-    enod = edof(i, 2:7);
+    e_nodes = edof(i, 2:7);
 
-    [es, et] = plants(Ex(i,:), Ey(i,:), [2 1], D, a(enod)' );
+    %calculate elementwise stress and strains
+    [es, et] = plants(Ex(i,:), Ey(i,:), [2 1], De, u(e_nodes)' );
     
+    %avarage temperature increase in an element
     enod2 = edof_old(i, 2:4);
-    Tnodes = Temp_stat(enod2);
-    dTavg = (Tnodes(1)+Tnodes(2)+Tnodes(3))/3 - 30;
+    T_nodes = Temp_stat(enod2);
+    dT_avg = (T_nodes(1)+T_nodes(2)+T_nodes(3))/3 - T_0;
     
-    De0 = alpha(i)*E(i)*dTavg/(1-2*nu(i)) * [1; 1; 0];
-    es = es-De0';
+    De_e0 = alpha(i)*E(i)*dT_avg/(1-2*nu(i)) * [1; 1; 0];
+    es = es-De_e0';
     
     sxx=es(1);
     syy=es(2);
     
+    
     G = 0.5*E(i)/(1+nu(i));
-    tao_xy = G*et(3); 
+    tau_xy = G*et(3); 
+    szz = nu(i)*(sxx+syy)-alpha(i)*E(i)*dT_avg; %13.42
     
-    szz = nu(i)*(sxx-syy)-alpha(i)*E(i)*dTavg;
-    
-    Seff_temp=sqrt(sxx^2+syy^2+szz^2-sxx*syy-sxx*szz-syy*szz+3*(tao_xy)^2);
-    Seff_el(i) = Seff_temp;
+    Seff_el(i) = sqrt(sxx^2+syy^2+szz^2-sxx*syy-sxx*szz-syy*szz+3*(tau_xy)^2); 
 end
 
+%get nodewise Seffs 
 Seff_nod = zeros(ndof/2, 1);
-
 for i=1: size(coord,1)
     [c0,c1] = find(edof_old(:,2:4)==i);
     Seff_nod(i) = sum(Seff_el(c0))/size(c0,1);
 end
 
-
-%Ed1=extract(Edof1,a);
-%Ed2=extract(Edof2,a);
-%[sfac]=scalfact2(Ex1,Ey1,Ed1,0.2);
-
-%eldisp2(Ex1,Ey1,Ed1,[2 1 1],sfac);
-%eldisp2(Ex2,Ey2,Ed2,[2 1 1],sfac);
-
-
 %PRINTA!!!
 figure(1)
 hold on
-Ed = extract(edof,a);
+Ed = extract(edof,u);
 eldraw2(Ex,Ey,[1,4,1]);
 eldraw2(-Ex,Ey,[1,4,1]);
 
@@ -125,3 +116,4 @@ Ed_neg = [-Ed(:,1),Ed(:,2), -Ed(:,3), Ed(:,4), -Ed(:,5), Ed(:,6)]
 
 scale = 'auto';
 draw_temps(Ex, Ey, edof_old, Seff_nod, 7, scale);
+
